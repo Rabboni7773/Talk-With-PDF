70d87e7bbae135b0a78268c956a77fd481a04260
Rabboni7773 <marabboni1@gmail.com>
2026-06-21 19:14:33 +0530

    Add RunnableWithMessageHistory import to rag_system.py

--- utils/rag_system.py
+++ utils/rag_system.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel
 from langchain_core.prompts import MessagesPlaceholder
